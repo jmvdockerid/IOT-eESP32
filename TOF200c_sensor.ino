f6e3651f0694d9f62116190bc23768b472d68899
Jan Visser <64650506+jmvdockerid@users.noreply.github.com>
2025-01-31 20:13:16 +0100

    Update TOF200c_sensor.ino
    Add info

--- TOF200c_sensor.ino
+++ TOF200c_sensor.ino
@@ -1,3 +1,15 @@
+/*
+ESP module: ESP-WROOM-32
+Getest met: Arduino IDE
+Sensor: TOF-200C  VL53L0X ;
+Doel: laser afstandmeting ;
+Specs: I2C, 3V3 ;
+Aansluiten (van sensor naar ESP)
+VCC naar 3.3V
+GND naar GND
+SDA naar GPIO 21
+SCL naar GPIO 22
+*/
 
 #include <Wire.h>
 #include <Adafruit_VL53L0X.h>
@@ -27,5 +39,5 @@ void loop() {
         Serial.println("Out of range.");
     }
 
-    delay(1000);
+    delay(1000); //delay between measurements
 }
